6a03affe20ae3b776390c05430b864d291018028
nodchip <nodchip@gmail.com>
2026-08-08 20:56:19 +0900

    Pin full-capacity benchmark worker

--- OpenBench/tests/test_source_pins.py
+++ OpenBench/tests/test_source_pins.py
@@ -13,7 +13,7 @@ class SourcePinTests(SimpleTestCase):
         self.assertEqual(config["client_version"], 41)
         self.assertEqual(
             config["client_repo_ref"],
-            "113b0db7d66ea408ebc3247f71a126539ab5f602",
+            "3e2abc27f541a9f18cba0d40c6c537dc0378fac1",
         )
         self.assertEqual(config["shogitest_min_version"], "0.1.2")
         self.assertEqual(
